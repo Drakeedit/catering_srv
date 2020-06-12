cceb6bf6d9c108082906138994ccc94796546200
parfeniukink <45270625+parfeniukink@users.noreply.github.com>
2020-06-12 19:20:45 +0300

    Update forms.py

--- catering/accounts/forms.py
+++ catering/accounts/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+# Чому ти тут використав класи, а у в'юхах - ф-ції??? Все через сраку! І де форма для Логіна. Тут тільки реєстрація....
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
